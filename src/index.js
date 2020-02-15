e53c3e41c6e0383c9e791d033915e3609f4bb749
Herman Wikner <herman.wikner@naf.no>
2020-02-15 16:12:03 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useCallback, createContext, useContext } from 'react';
+
+const ScrollInfo = createContext();
+
+export const ScrollInfoProvider = ({ children }) => {
+
+    const [direction, setDirection] = useState(0);
+    const [pos, setPos] = useState(0);
+
+    const handleScroll = useCallback(() => {
+        if ((document.body.getBoundingClientRect()).top > pos) {
+            setDirection('UP')
+        }
+        else {
+            setDirection('DOWN')
+        }
+        setPos((document.body.getBoundingClientRect()).top);
+    }, [pos])
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [handleScroll])
+
+    return (
+        <ScrollInfo.Provider value={direction}>
+            {children}
+        </ScrollInfo.Provider>
+    );
+}
+
+export const useScrollDirection = () => {
+    const direction = useContext(ScrollInfo)
+    return direction;
+}
+
+
